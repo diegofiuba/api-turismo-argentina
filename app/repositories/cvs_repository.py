691ba207ea9fb4b4e6579ed45275b4e4a566b418
diegofiuba <diegotournet@gmail.com>
2026-04-08 13:56:07 -0300

    feature: add CSV repository for loading places data

---
+++ app/repositories/cvs_repository.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+class CSVRepository:
+   def __init__(self,file_path: str):
+       self.df = pd.read_csv(file_path)
+
+   """ Return places filter by key and value passed by argument """
+   def filter(self, **kwargs):
+       df = self.df
+       for key, value in kwargs.items():
+           if value:
+               df = df[df[key].str.lower() == value.lower()]
+       return df
+
+   """ Return places that contains the text passed by argument """
+   def search(self,query:str):
+       return self.df[self.df["name"].str.contains(query,case=False,na=False)] 
