3c2cf25b6d3da3dfcbb7e0ec7196291565fb4069
diegofiuba <diegotournet@gmail.com>
2026-04-09 00:57:53 -0300

    feature: add filtering by province and city to places endpoint

--- app/routes/places.py
+++ app/routes/places.py
@@ -6,3 +6,10 @@ router = APIRouter()
 @router.get("/",description="Get a list of places limited by parameter")
 def get_places(limit: int = Query(5,description="Limit of places to list")):
     return PlaceService.get_places(limit)
+
+@router.get("/filter",description="Get a list of places associated to the province and city passed by parameter")
+def get_places(province:str = Query(None,description="province"),
+               city:str = Query(None,description="city")  
+
+):
+    return PlaceService.filter_places(province,city)
--- app/services/place_service.py
+++ app/services/place_service.py
@@ -5,15 +5,8 @@ from app.repositories.csv_repository import CSVRepository
 repo = CSVRepository("data/lugares_turisticos_argentina.csv")
 
 class PlaceService:
-   
-   """ Return the places limited by limit """
    @staticmethod
-   def get_places(limit: int = 5):
-       df = repo.get_places(limit)
-
-       #df = df.where(pd.notnull(df), None)
-       records = df.to_dict(orient="records")
-
+   def __clean(records):
        clean_records = [] 
        for row in records:
            clean_row = {}
@@ -22,5 +15,19 @@ class PlaceService:
                    clean_row[k] = None
                else:
                    clean_row[k] = v
-           clean_records.append(clean_row)  
-       return clean_records 
+           clean_records.append(clean_row) 
+       return clean_records    
+
+   """ Return the places limited by limit """
+   @staticmethod
+   def get_places(limit: int = 5):
+       df = repo.get_places(limit)
+       records = df.to_dict(orient="records")
+       return PlaceService.__clean(records) 
+
+   """ Return the places associated to the specific province and/or city """
+   @staticmethod
+   def filter_places(province: str = None, city: str = None):
+       df = repo.filter(province = province,city = city)
+       records = df.to_dict(orient="records")
+       return PlaceService.__clean(records)   
