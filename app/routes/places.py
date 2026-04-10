5a9ed746e585139909f7db947634726f360810da
diegofiuba <diegotournet@gmail.com>
2026-04-09 21:24:55 -0300

    refactor: unify filtering, search and nearby into /places endpoint

--- app/routes/places.py
+++ app/routes/places.py
@@ -3,9 +3,15 @@ from app.services.place_service import PlaceService
 
 router = APIRouter()
 
-@router.get("/",description="Get a list of places limited by parameter")
-def get_places(limit: int = Query(5,description="Limit of places to list")):
-    return PlaceService.get_places(limit)
+@router.get("/",description="Get a list of places")
+def get_places(limit: int = Query(10,description="Limit of places to list"),
+               province:str = Query(None,description="province of the places to filter"),
+               city:str = Query(None,description="city of the places to filter"),
+               keywords: str = Query(None,description="keywords to find a place"),
+               latitude: float = Query(None,description="latitude to find nearby places"),
+               longitude: float = Query(None,description="longitude to find nearby places"), 
+               radius: float = Query(10,description="radius to find nearby places")):
+    return PlaceService.get_places(limit,province,city,keywords,latitude,longitude,radius)
 
 @router.get("/filter",description="Get a list of places associated to the province and city passed by parameter")
 def get_places(province:str = Query(None,description="province"),
